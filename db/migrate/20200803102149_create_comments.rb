5a8c012acb2a1fab51d79e405b66e7f1d32a0fa1
youki0607 <its_showtime@i.softbank.jp>
2020-09-22 16:02:26 +0900

    migrate実行

--- db/migrate/20200803102149_create_comments.rb
+++ db/migrate/20200803102149_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :tweet_id
-      t.text :text
+      t.integer :user_id,  null: false
+      t.integer :tweet_id, null: false
+      t.text    :text,     null: false
       t.timestamps
     end
   end
